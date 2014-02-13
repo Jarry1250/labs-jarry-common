044779dda99a6a64141f8d4f2b3c98022d79c436
jarry1250 <jarry1250@gmail.com>
2014-02-13 22:28:44 +0000

    Update lineup on homepage

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	 */
 	require_once( '/data/project/jarry-common/public_html/global.php' );
 	
-	echo get_html( 'header', 'Introduction' );
+	echo get_html( 'header', _html( 'homepage-title', 'jarry' ) );
 ?>
 			<p>Welcome to my index of Wikimedia Labs tools I (User:Jarry1250) have been involved in the development of. If you have any queries, you can always contact me by leaving a message on <a href="//en.wikipedia.org/wiki/User_talk:Jarry1250">my en.wp talk page</a> or by following that page's "Email this user" link. <em>Note that tools may be periodically removed. If you rely on one that does get removed, just tell me and I will replace it for you.</em> The last such rotation was in December 2013, during the transition to Labs.</p>
 		</div>
@@ -29,11 +29,12 @@
 		<div class="boxes-holder">
 			<ul class="boxes">
 				<li><h3><a href="http://tools.wmflabs.org/wikicup/">WikiCup</a></h3><p>Live scores, plus individual and competition totals.</p><p class="source"><a href="https://github.com/Jarry1250/labs-wikicup"><?= _html( 'view-source' , 'general' ); ?></p></li>
-				<li><h3><a href="http://toolserver.org/~jarry/svgtranslate/">SVG Translate</a></h3><p>(Non-functional.) Translate an SVG into a language or your choice.</p></li>
+				<li><h3><a href="http://toolserver.org/~jarry/svgtranslate/">SVG Translate</a></h3><p>(Current non-functional.) Translate an SVG into a language or your choice.</p></li>
 				<li><h3><a href="http://tools.wmflabs.org/svgcheck/">SVG Check</a></h3><p>Preview the display of SVG files and detect errors.</p><p class="source"><a href="https://github.com/Jarry1250/labs-svgcheck"><?= _html( 'view-source' , 'general' ); ?></p></li>
+				<li><h3><a href="http://tools.wmflabs.org/grep/">Grep</a></h3><p>Search page titles on a wiki using regular expressions.</p><p class="source"><a href="https://github.com/Jarry1250/labs-grep"><?= _html( 'view-source' , 'general' ); ?></p></li>
 				<li><h3><a href="http://tools.wmflabs.org/wmukevents/">Wikimedia UK events</a></h3><p>Auto-generated .ics (calendar) file</p><p class="source"><a href="https://github.com/Jarry1250/labs-wmukevents"><?= _html( 'view-source' , 'general' ); ?></p></li>
 				<li><h3><a href="http://tools.wmflabs.org/signpost/">Signpost publishing</a></h3><p>Tools built to assist the Signpost's Editor-in-Chief</p><p class="source"><a href="https://github.com/Jarry1250/labs-signpost"><?= _html( 'view-source' , 'general' ); ?></p></li>
+				<li><h3>Scripts</h3><p>Scripts underlying other tasks performed by my bots</p><p class="source"><a href="https://github.com/Jarry1250/labs-jarry-common"><?= _html( 'view-source' , 'general' ); ?></p></li>
 			</ul>
 <?php
 	echo get_html( 'footer' );
-?>
