586be17addb16206cdad1df05c22462437f955ba
jarry1250 <jarry1250@gmail.com>
2014-04-06 16:23:25 +0100

    Change diffing library

--- libs/Diff.php
+++ libs/Diff.php
@@ -1,88 +1,74 @@
 <?php
-	// Copyright (C) 2012 Jacob Barkdull
-	//  and 2014 Harry Burt
-	//
-	//  This program is free software: you can redistribute it and/or modify
-	//  it under the terms of the GNU Affero General Public License as
-	//  published by the Free Software Foundation, either version 3 of the
-	//  License, or (at your option) any later version.
-	//
-	//  This program is distributed in the hope that it will be useful,
-	//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	//  GNU Affero General Public License for more details.
-	//
-	//  You should have received a copy of the GNU Affero General Public License
-	//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	/*
+		Paul's Simple Diff Algorithm v 0.1
+		(C) Paul Butler 2007 <http://www.paulbutler.org/>
+		May be used and distributed under the zlib/libpng license.
+		This code is intended for learning purposes; it was written with short
+		code taking priority over performance. It could be used in a practical
+		application, but there are a few ways it could be optimized.
+		Given two arrays, the function diff will return an array of the changes.
+		I won't describe the format of the array, but it will be obvious
+		if you use print_r() on the result of a diff on some test data.
+		htmlDiff is a wrapper for the diff command, it takes two strings and
+		returns the differences in HTML. The tags used are <ins> and <del>,
+		which can easily be styled with CSS.
+	*/
 
 	class Diff {
 		private $html;
+
 		function __construct( $before, $after ) {
-			// HTML Table
-			$this->html = '<table width="100%">' . "\n<tbody>\n<tr>\n";
-			$this->html .= '<td valign="top">' . "\nCurrent Version:<hr>\n<pre>\n";
+			$before = preg_split( "/[\r\n]+/", $before );
+			$after = preg_split( "/[\r\n]+/", $after );
+			
+			$diff = $this->diff( $before, $after );
 
-			// Line Arrays
-			$cv = explode( "\n", htmlspecialchars( $before ) ); // Current Version
-			$ov = explode( "\n", htmlspecialchars( $after ) ); // Old Version
-
-			// Count Lines - Set to Longer Version
-			$lc = ( count( $cv ) > count( $ov ) ) ? count( $cv ) : count( $ov );
-
-			// Fix Mismatched Line Counts
-			for( $flc = count( $ov ); $flc != $lc; $flc++ ){
-				$ov["$flc"] = '';
-			}
-
-			for( $l = '0'; $l != $lc; $l++ ){
-				// Word Arrays
-				$cw = explode( ' ', $cv["$l"] ); // Current Version
-				$ow = explode( ' ', $ov["$l"] ); // Old Version
-
-				// Count Words - Set to Longer Version
-				$wc = ( count( $cw ) > count( $ow ) ) ? count( $cw ) : count( $ow );
-
-				// Fix Mismatched Word Counts
-				for( $fwc = count( $ow ); $fwc != $wc; $fwc++ ){
-					$ow["$fwc"] = '';
-				}
-
-				// If each line is identical, just $this->html .= the normal line. If not,
-				// check if each word is identical. If not, wrap colored "<b>"
-				// tags around the mismatched words.
-				if( $cv["$l"] !== $ov["$l"] ){
-					for( $w = '0'; $w != $wc; $w++ ){
-						if( $cw["$w"] === $ow["$w"] ){
-							$this->html .= $cw["$w"];
-							$this->html .= ( $w != ( $wc - 1 ) ) ? ' ' : "\n";
-						} else {
-							$this->html .= '<b style="color: #00BB00;">' . $cw["$w"];
-							$this->html .= ( $w != ( $wc - 1 ) ) ? '</b> ' : "</b>\n";
-						}
-					}
+			$this->html = '';
+			foreach( $diff as $k ){
+				if( is_array( $k ) ){
+					$this->html .= ( !empty( $k['d'] ) ? "<del>" . implode( ' ', $k['d'] ) . "</del> " : '' ) .
+					        ( !empty( $k['i'] ) ? "<ins>" . implode( ' ', $k['i'] ) . "</ins> " : '' );
 				} else {
-					$this->html .= $cv["$l"] . "\n";
+					$this->html .= $k . ' ';
 				}
 			}
-
-			// Ending HTML Tags
-			$this->html .= "</pre>\n</td>\n<td>&nbsp;</td>\n";
-			$this->html .= '<td valign="top">' . "\nOld Version:<hr>\n<pre>\n";
-
-			// Read and Display Old Version
-			$this->html .= implode( "\n", $ov );
-			$this->html .= "</pre>\n</td>\n</tr>\n</tbody>\n</table>";
+		}
+	
+		private function diff ( $before, $after ) {
+			$matrix = array();
+			$maxlen = 0;
+			foreach( $before as $oindex => $ovalue ){
+				$nkeys = array_keys( $after, $ovalue );
+				foreach( $nkeys as $nindex ){
+					$matrix[$oindex][$nindex] = isset( $matrix[$oindex - 1][$nindex - 1] ) ?
+						$matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+					if( $matrix[$oindex][$nindex] > $maxlen ){
+						$maxlen = $matrix[$oindex][$nindex];
+						$omax = $oindex + 1 - $maxlen;
+						$nmax = $nindex + 1 - $maxlen;
+					}
+				}
+			}
+			if( $maxlen == 0 ){
+				return array( array( 'd' => $before, 'i' => $after ) );
+			}
+			return array_merge(
+				$this->diff( array_slice( $before, 0, $omax ), array_slice( $after, 0, $nmax ) ),
+				array_slice( $after, $nmax, $maxlen ),
+				$this->diff( array_slice( $before, $omax + $maxlen ), array_slice( $after, $nmax + $maxlen ) )
+			);
 		}
 
-		function getDiff () {
+		function getDiff() {
 			return $this->html;
 		}
 
-		function printDiff () {
+		function printDiff() {
 			echo $this->html;
 		}
 
-		static function load( $before, $after ){
+		static function load( $before, $after ) {
 			return new Diff( $before, $after );
 		}
 	}
